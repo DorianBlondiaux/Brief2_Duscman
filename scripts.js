17de05c783846ef29257b5f913921396fc9fa67e
Blondiaux Dorian <dorian5959@gmail.com>
2023-09-25 16:26:08 +0200

    Ajout articles par categorie

--- scripts.js
+++ scripts.js
@@ -1,18 +1,43 @@
 let jsonArticles;
 
+//Creation des cards pour tout les articles 
+getJSON('articles.json').then(data => {
+    jsonArticles = data;  
+    document.getElementById('card').innerHTML = jsonArticles.articles.map(article => 
+        ` 
+        <div class="card-body">
+          <img src="https://placehold.co/600x400" class="img_item" alt="...">
+          <h5>Nom: ${article.nom}</h5>
+          <p>Prix: ${article.prix}</p>
+          <p>${article.designation}</p>
+          <p class="categorie">${article.categorie}</p>
+          <a href="#" class="btn btn-primary">Ajouter</a>
+        </div>
+        `
+    ).join('')
+})
+
 function getJSON(path) {
     return fetch(path).then(response => response.json());
 }
 
-getJSON('articles.json').then(data => {
-    jsonArticles = data;  
-    document.getElementById('card').innerHTML = jsonArticles.articles.map(article => 
-        `  
-          <img src="https://placehold.co/600x400" class="img_item" alt="...">
-          <h5>Name: ${article.nom}</h5>
-          <p>Age: ${article.prix}</p>
-          <p>Place: ${article.designation}</p>
-        `
-    ).join('')
-})
+function displayOneArticle(categorie){
+    //On recupere toutes les cards et on boucle dessus
+    document.querySelectorAll('.card-body').forEach(elem => {
+    
+    //On boucle sur les categorie de chaque card pour obtenir sa valeur
+    let isCategorie = false;
+    elem.querySelectorAll('.categorie').forEach(elem2 => {
+;         isCategorie = elem2.childNodes[0].nodeValue== categorie;
+    })
+
+    //Si c'est la bonne categorie on l'affiche, sinon on la cache
+    if(isCategorie){
+        elem.style.display = 'block';
+    }else{
+        elem.style.display = 'none';
+    }
+    });
+}
+
 
